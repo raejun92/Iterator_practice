e004715a6c74bf9895970fcea914f101511cecd5
raejun92 <cvcv1324@naver.com>
2020-07-23 14:58:21 +0900

    1추가 2삭제 3검색 4목록 0종료 인터페이스

---
+++ src/Member.java
@@ -0,0 +1,20 @@
+
+public class Member {
+
+	final int num;
+	String name;
+
+	public Member(int num, String name) {
+		this.num = num;
+		this.name = name;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public String toString() {
+		return String.format("번호:%d 이름:%s", num, name);
+	}
+
+}
